5b78c71cf43a4fad3a3c61c765bf511dee23c9d9
Davi de Lima <davidelima1515@gmail.com>
2020-05-05 04:43:56 -0300

    atualização no app st

--- streamlit-app-0/main.py
+++ streamlit-app-0/main.py
@@ -7,17 +7,20 @@ def main():
     if name:
         st.write('Bem-vindo(a), ', name,'!!!!')
         st.write('É um prazer recebê-lo(a) aqui.')
+        carregando_arq = st.file_uploader('Entre com o arquivo com a qual deseja trabalhar: ', type='csv')
+        if carregando_arq:
+            leitor = pd.read_csv(carregando_arq)
+            st.write(leitor.head(10))
+            st.sidebar.text('O QUE VOCÊ QUER VER AGORA? ')
+            st.sidebar.selectbox('ESCOLHA DENTRE AS OPÇÕES ABAIXO: ', ('PRÉ-PROCESSAMENTO DE DADOS', 'ANALISE DE DADOS', 'REGRESSÃO'))
+            if 'PRÉ-PROCESSAMENTO DE DADOS':
+                st.header('PRÉ-PROCESSAMENTO DE DADOS!!!!')
+                st.subheader('DADOS')
+                st.write(leitor.isna().sum())
 
-    carregando_arq = st.file_uploader('Entre com o arquivo com a qual deseja trabalhar: ', type='csv')
-    if carregando_arq:
-        leitor = pd.read_csv(carregando_arq)
-        
-
-    st.header('PRÉ-PROCESSAMENTO DE DADOS!!!!')
-    if carregando_arq:
-        leitor.columns
-        
-
+            
+         
+            
 
 
 if __name__ == "__main__":
